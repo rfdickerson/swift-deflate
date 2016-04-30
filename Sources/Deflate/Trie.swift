bbe2430fe359d298cef83befdc40f2321352bca2
Robert F. Dickerson <rfdickerson@us.ibm.com>
2016-04-30 17:51:36 -0500

    Adding Trie data structure

--- Sources/Deflate/Trie.swift
+++ Sources/Deflate/Trie.swift
@@ -7,3 +7,61 @@
 //
 
 import Foundation
+
+class TrieNode {
+    
+    var children = [TrieNode]()
+    
+    var final: Bool = false
+    
+    var value: Byte?
+    
+    
+    
+}
+
+extension TrieNode {
+    
+    func insert(bytes: [Byte]) {
+     
+        var currentNode = self
+        
+        for byte in bytes {
+            
+            // search for child if not exists, then add it 
+            var found = false
+            
+            for child in currentNode.children {
+                
+                if child.value == byte {
+                    
+                    currentNode = child
+                    found = true
+                    
+                } else {
+                    
+                    
+                }
+            }
+            
+            if !found {
+                
+                let newNode = TrieNode()
+                newNode.value = byte
+                currentNode.children.append( newNode )
+                
+                currentNode = newNode
+
+            }
+
+            
+        }
+        
+        
+    }
+    
+    func contains(bytes: [Byte]) -> Bool {
+        
+        return true
+    }
+}
--- Tests/Deflate/TestTrie.swift
+++ Tests/Deflate/TestTrie.swift
@@ -6,4 +6,27 @@
 //
 //
 
+import XCTest
+
 import Foundation
+
+@testable import Deflate
+
+
+class TestTrie : XCTestCase {
+
+    func testInsert() {
+        let input: [Byte] = [0x80, 0x80, 0x80, 0x80, 0x40]
+        let input2: [Byte] = [0x80, 0x80, 0x80, 0x80, 0x42]
+
+        
+        let trie = TrieNode()
+        trie.insert(bytes: input)
+        
+        trie.insert(bytes: input2)
+        
+        XCTAssertEqual(trie.final, false)
+        
+        
+    }
+}
